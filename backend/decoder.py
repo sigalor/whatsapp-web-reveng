3d74147ecf6328628ebc1f5649f4cc9e27b25090
Abyr Valg <valga@abyrga.ru>
2018-05-14 11:38:11 +0300

    Fix tokens handling

--- backend/decoder.py
+++ backend/decoder.py
@@ -14,28 +14,29 @@ connInfo = {
 	"me": "1234567890@c.us"
 };
 
-Tokens = [None,None,None,"200","400","404","500","501","502","action","add",
-		  "after","archive","author","available","battery","before","body",
-		  "broadcast","chat","clear","code","composing","contacts","count",
-		  "create","debug","delete","demote","duplicate","encoding","error",
-		  "false","filehash","from","g.us","group","groups_v2","height","id",
-		  "image","in","index","invis","item","jid","kind","last","leave",
-		  "live","log","media","message","mimetype","missing","modify","name",
-		  "notification","notify","out","owner","participant","paused",
-		  "picture","played","presence","preview","promote","query","raw",
-		  "read","receipt","received","recipient","recording","relay",
-		  "remove","response","resume","retry","s.whatsapp.net","seconds",
-		  "set","size","status","subject","subscribe","t","text","to","true",
-		  "type","unarchive","unavailable","url","user","value","web","width",
-		  "mute","read_only","admin","creator","short","update","powersave",
-		  "checksum","epoch","block","previous","409","replaced","reason",
-		  "spam","modify_tag","message_info","delivery","emoji","title",
-		  "description","canonical-url","matched-text","star","unstar",
-		  "media_key","filename","identity","unread","page","page_count",
-		  "search","media_message","security","call_log","profile","ciphertext",
-		  "invite","gif","vcard","frequent","privacy","blacklist","whitelist",
-		  "verify","location","document","elapsed","revoke_invite","expiration",
-		  "unsubscribe","disable"];
+SingleByteTokens = [None,None,None,"200","400","404","500","501","502","action","add",
+					"after","archive","author","available","battery","before","body",
+					"broadcast","chat","clear","code","composing","contacts","count",
+					"create","debug","delete","demote","duplicate","encoding","error",
+					"false","filehash","from","g.us","group","groups_v2","height","id",
+					"image","in","index","invis","item","jid","kind","last","leave",
+					"live","log","media","message","mimetype","missing","modify","name",
+					"notification","notify","out","owner","participant","paused",
+					"picture","played","presence","preview","promote","query","raw",
+					"read","receipt","received","recipient","recording","relay",
+					"remove","response","resume","retry","s.whatsapp.net","seconds",
+					"set","size","status","subject","subscribe","t","text","to","true",
+					"type","unarchive","unavailable","url","user","value","web","width",
+					"mute","read_only","admin","creator","short","update","powersave",
+					"checksum","epoch","block","previous","409","replaced","reason",
+					"spam","modify_tag","message_info","delivery","emoji","title",
+					"description","canonical-url","matched-text","star","unstar",
+					"media_key","filename","identity","unread","page","page_count",
+					"search","media_message","security","call_log","profile","ciphertext",
+					"invite","gif","vcard","frequent","privacy","blacklist","whitelist",
+					"verify","location","document","elapsed","revoke_invite","expiration",
+					"unsubscribe","disable"];
+DoubleByteTokens = [];
 
 class Tags:
 	LIST_EMPTY   = 0;
@@ -271,15 +272,15 @@ class MessageParser:
 		return ret;
 	
 	def getToken(self, index):
-		if index < 0 or index >= len(Tokens):
+		if index < 3 or index >= len(SingleByteTokens):
 			raise ValueError("invalid token index: " + str(index));
-		return Tokens[index];
+		return SingleByteTokens[index];
 
 	def getTokenDouble(self, index1, index2):
 		n = 256 * index1 + index2;
-		if n < 0 or n >= len(Tokens):
+		if n < 0 or n >= len(DoubleByteTokens):
 			raise ValueError("invalid token index: " + str(n));
-		return Tokens[n];
+		return DoubleByteTokens[n];
 
 
 
